c8867b2ab4d5b3782ff546c505c19eefcee2d9f3
shubhamkalsait <shubhamkalsait@cloudblitz.in>
2022-04-01 10:42:10 +0530

    --- Auto-Git Commit ---

--- pipeline/scriptedPipeline.groovy
+++ pipeline/scriptedPipeline.groovy
@@ -1,9 +1,9 @@
 node {  
     stage('Pull') { 
-        // 
+        git 'https://github.com/shubhamkalsait/studentapp-ui.git'
     }
     stage('Build') { 
-        // 
+        echo ''
     }
     stage('Test') { 
         // 
