3ba6f799ae0671d65018baf47fa534872cdbcc99
shubhamkalsait <shubhamkalsait@cloudblitz.in>
2022-04-05 10:35:52 +0530

    --- Auto-Git Commit ---

--- seedjobs/seedjob2.groovy
+++ seedjobs/seedjob2.groovy
@@ -8,7 +8,7 @@ pipelineJob('scripted2') {
 
     flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps@2683.vd0a_8f6a_1c263') {
 
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git@4.11.0') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
 
         'configVersion'(2)
 
