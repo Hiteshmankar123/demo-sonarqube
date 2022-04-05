ac739fc3a8ff5825833f871064d306285c6300ea
shubhamkalsait <shubhamkalsait@cloudblitz.in>
2022-04-05 10:31:51 +0530

    --- Auto-Git Commit ---

---
+++ seedjobs/seedjob2.groovy
@@ -0,0 +1,63 @@
+pipelineJob('scriptedPipeline_converted') {
+
+  description('')
+
+  displayName('scriptedPipeline_converted')
+
+  keepDependencies(false)
+
+  quietPeriod(0)
+
+  checkoutRetryCount(0)
+
+  disabled(false)
+
+  concurrentBuild(false)
+
+  configure { flowdefinition ->
+
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps@2683.vd0a_8f6a_1c263') {
+
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git@4.11.0') {
+
+        'configVersion'(2)
+
+        'userRemoteConfigs' {
+
+          'hudson.plugins.git.UserRemoteConfig' {
+
+            'url'('https://gitlab.com/shubham.kalsait/b16-devops.git')
+
+            'credentialsId'('gitlab-user-pwd')
+
+          }
+
+        }
+
+        'branches' {
+
+          'hudson.plugins.git.BranchSpec' {
+
+            'name'('*/main')
+
+          }
+
+        }
+
+        'doGenerateSubmoduleConfigurations'(false)
+
+        'submoduleCfg'(class:'empty-list')
+
+        'extensions'()
+
+      }
+
+      'scriptPath'('pipeline/scriptedPipeline.groovy')
+
+      'lightweight'(true)
+
+    }
+
+  }
+
+}
